326d09aeab9f908cf8365ef7f9808642199483d4
Eder Benjamin <ben-eder@freenet.de>
2021-01-19 12:55:23 +0100

    Created page to show the articles of DrinksOnDemand

--- src/main/java/de/othr/sw/benjamineder/barmanagement/application/web/admin/AdminSimpleController.java
+++ src/main/java/de/othr/sw/benjamineder/barmanagement/application/web/admin/AdminSimpleController.java
@@ -3,25 +3,25 @@ package de.othr.sw.benjamineder.barmanagement.application.web.admin;
 import de.othr.sw.benjamineder.barmanagement.application.drink.entity.SimpleDrink;
 import de.othr.sw.benjamineder.barmanagement.application.drink.entity.SimpleDrinkType;
 import de.othr.sw.benjamineder.barmanagement.application.drink.service.SimpleDrinkService;
-import java.util.UUID;
+import de.othr.sw.benjamineder.barmanagement.application.rest.drinksondemand.DrinksOnDemandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin/simple")
 public class AdminSimpleController {
 
-  private final SimpleDrinkService simpleDrinkService;
+  private final SimpleDrinkService    simpleDrinkService;
+  private final DrinksOnDemandService drinksOnDemandService;
 
   @Autowired
-  public AdminSimpleController(SimpleDrinkService simpleDrinkService) {
+  public AdminSimpleController(SimpleDrinkService simpleDrinkService, DrinksOnDemandService drinksOnDemandService) {
     this.simpleDrinkService = simpleDrinkService;
+    this.drinksOnDemandService = drinksOnDemandService;
   }
 
   @GetMapping(path = "/{drinkId}")
@@ -39,5 +39,12 @@ public class AdminSimpleController {
          .addAttribute("types", SimpleDrinkType.values())
          .addAttribute("saved", true);
     return "admin_simple";
+  }
+
+  @GetMapping(path = "/drinks-on-demand")
+  public String adminDrinksOnDemandArticles(Model model) {
+    var articles = drinksOnDemandService.getArticles();
+    model.addAttribute("articles", articles);
+    return "admin_drinks-on-demand";
   }
 }
