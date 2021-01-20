54816157d9f0aa9e64441b9e47c6ed58cff2252c
Eder Benjamin <ben-eder@freenet.de>
2021-01-20 18:38:22 +0100

    Created recipe book page which shows the notes of the recipe if present

---
+++ src/main/java/de/othr/sw/benjamineder/barmanagement/application/web/bar/RecipeBookController.java
@@ -0,0 +1,31 @@
+package de.othr.sw.benjamineder.barmanagement.application.web.bar;
+
+import de.othr.sw.benjamineder.barmanagement.application.drink.service.ComplexDrinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/recipe-book")
+public class RecipeBookController {
+
+  private final ComplexDrinkService complexDrinkService;
+
+  @Autowired
+  public RecipeBookController(ComplexDrinkService complexDrinkService) {
+    this.complexDrinkService = complexDrinkService;
+  }
+
+  @GetMapping
+  public String recipeOverview(Model model) {
+    var drinksWithRecipe = complexDrinkService.getDrinks().stream()
+                                              .filter(drink -> drink.getRecipe() != null)
+                                              .collect(Collectors.toList());
+    model.addAttribute("drinksWithRecipe", drinksWithRecipe);
+    return "bar_recipe-book";
+  }
+}
