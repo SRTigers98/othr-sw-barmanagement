9cc117c321e126ab98124e660777886d1cb62d95
Eder Benjamin <ben-eder@freenet.de>
2021-01-22 15:26:00 +0100

    Fixed logout error

--- src/main/java/de/othr/sw/benjamineder/barmanagement/application/auth/config/AuthConfiguration.java
+++ src/main/java/de/othr/sw/benjamineder/barmanagement/application/auth/config/AuthConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -40,7 +41,7 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
         .failureUrl("/login?error=true")
         .and()
         .logout()
-        .logoutUrl("/logout")
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
         .deleteCookies("JSESSIONID");
 
     http.cors()
