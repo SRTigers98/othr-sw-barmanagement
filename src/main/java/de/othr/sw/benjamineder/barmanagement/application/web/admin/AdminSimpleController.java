0121a26de363edf9439eb4291e6b5489e8cc9974
Eder Benjamin <ben-eder@freenet.de>
2021-01-25 11:08:52 +0100

    fix: SimpleDrinks can only be deleted if they are deletable

--- src/main/java/de/othr/sw/benjamineder/barmanagement/application/web/admin/AdminSimpleController.java
+++ src/main/java/de/othr/sw/benjamineder/barmanagement/application/web/admin/AdminSimpleController.java
@@ -2,7 +2,9 @@ package de.othr.sw.benjamineder.barmanagement.application.web.admin;
 
 import de.othr.sw.benjamineder.barmanagement.application.drink.entity.SimpleDrink;
 import de.othr.sw.benjamineder.barmanagement.application.drink.entity.SimpleDrinkType;
+import de.othr.sw.benjamineder.barmanagement.application.drink.service.ComplexDrinkService;
 import de.othr.sw.benjamineder.barmanagement.application.drink.service.SimpleDrinkService;
+import de.othr.sw.benjamineder.barmanagement.application.order.service.DrinkOrderService;
 import de.othr.sw.benjamineder.barmanagement.application.rest.drinksondemand.DrinksOnDemandService;
 import de.othr.sw.benjamineder.barmanagement.application.web.auth.BarAdminAccess;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +18,16 @@ public class AdminSimpleController {
 
   private final SimpleDrinkService    simpleDrinkService;
   private final DrinksOnDemandService drinksOnDemandService;
+  private final ComplexDrinkService   complexDrinkService;
+  private final DrinkOrderService     drinkOrderService;
 
   @Autowired
-  public AdminSimpleController(SimpleDrinkService simpleDrinkService, DrinksOnDemandService drinksOnDemandService) {
+  public AdminSimpleController(SimpleDrinkService simpleDrinkService, DrinksOnDemandService drinksOnDemandService,
+                               ComplexDrinkService complexDrinkService, DrinkOrderService drinkOrderService) {
     this.simpleDrinkService = simpleDrinkService;
     this.drinksOnDemandService = drinksOnDemandService;
+    this.complexDrinkService = complexDrinkService;
+    this.drinkOrderService = drinkOrderService;
   }
 
   @BarAdminAccess
@@ -30,7 +37,8 @@ public class AdminSimpleController {
                                   .orElseGet(() -> drinksOnDemandService.getArticleAsSimpleDrink(drinkId));
     model.addAttribute("drink", drink)
          .addAttribute("types", SimpleDrinkType.values())
-         .addAttribute("saved", false);
+         .addAttribute("saved", false)
+         .addAttribute("deletable", isDrinkDeletable(drinkId));
     return "admin/admin_simple";
   }
 
@@ -40,7 +48,8 @@ public class AdminSimpleController {
     var savedDrink = simpleDrinkService.addOrUpdateDrink(drink);
     model.addAttribute("drink", savedDrink)
          .addAttribute("types", SimpleDrinkType.values())
-         .addAttribute("saved", true);
+         .addAttribute("saved", true)
+         .addAttribute("deletable", isDrinkDeletable(drink.getId()));
     return "admin/admin_simple";
   }
 
@@ -49,5 +58,16 @@ public class AdminSimpleController {
   public String adminDeleteSimpleDrink(@PathVariable("drinkId") String drinkId) {
     simpleDrinkService.deleteDrink(drinkId);
     return "redirect:/admin";
+  }
+
+  private boolean isDrinkDeletable(String drinkId) {
+    var isDrinkPresent = simpleDrinkService.getDrinkById(drinkId).isPresent();
+    var isRecipeComponent = complexDrinkService.getDrinks().stream()
+                                               .flatMap(drink -> drink.getRecipe().getComponents().stream())
+                                               .anyMatch(component -> component.getComponent().getId().equals(drinkId));
+    var isOrdered = drinkOrderService.getDrinkOrders().stream()
+                                     .flatMap(order -> order.getOrderPositions().stream())
+                                     .anyMatch(position -> position.getDrink().getId().equals(drinkId));
+    return isDrinkPresent && !(isRecipeComponent || isOrdered);
   }
 }
